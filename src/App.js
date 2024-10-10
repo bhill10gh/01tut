7d80d6f58f5f4a10fd9d55f0fc9969bb0352e34a
Barry Hill <Bhill@oattravel.com>
2024-10-10 12:33:23 -0400

    check

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.  Also want to save to see it change
+          eidit
         </p>
         <a
           className="App-link"
