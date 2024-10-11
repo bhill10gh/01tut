e37d11ffc7c5df7b793cc8312f33184d591dc391
Barry Hill <Bhill@oattravel.com>
2024-10-11 12:33:23 -0400

    Chapter 8

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Header = props => {
-    
+const Header = ({title = "Default Tilte"}) => {
+
   return (
     <header>
-      <h1>Groceries List</h1>
+      <h1>{title}</h1>
     </header>
   )
 }
